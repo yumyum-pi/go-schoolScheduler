803caf96d3682f168da7940ba0a39a971c623483
vivek rawat <vivekrawat@viveks-MacBook-Pro.local>
2020-03-20 13:35:59 +0530

    created data package. Added Get Function to read data from json files.

---
+++ data/get.go
@@ -0,0 +1,26 @@
+package data
+
+import (
+	"github.com/yumyum-pi/go-schoolScheduler/models"
+	"github.com/yumyum-pi/go-schoolScheduler/utils"
+)
+
+//var classes []models.Class
+
+/*
+Getreturn the following data:
+ - Array of classes
+ - Array of teacher
+This func is reading from local file
+*/
+func Get() (classes []models.Class, teachers []models.Teacher) {
+	// TODO get data from database
+
+	// get class data
+	getClass(&classes)
+	return
+}
+
+func getClass(c *[]models.Class) {
+	utils.ReadFile(utils.ResourceFilePath("classes"), &c)
+}
