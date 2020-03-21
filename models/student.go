e768c3d73c3bcfc34e13e831661ad687c3af6c1f
vivek rawat <vivekrawat@viveks-MacBook-Pro.local>
2020-03-21 15:46:13 +0530

    Added StudentID to the Sudent struct.

--- models/student.go
+++ models/student.go
@@ -9,8 +9,7 @@ type StudentID struct {
 
 // Student struct hold's student data
 type Student struct {
-	// TODO make a proper ID just like classID
-	ID      int     `json:"id"`      //Unique identifier for the student
-	Name    Name    `json:"name"`    // Name object of the student
-	ClassID ClassID `json:"classID"` // Unique identifier the students class
+	ID      StudentID `json:"id"`      //Unique identifier for the student
+	Name    Name      `json:"name"`    // Name object of the student
+	ClassID ClassID   `json:"classID"` // Unique identifier the students class
 }
