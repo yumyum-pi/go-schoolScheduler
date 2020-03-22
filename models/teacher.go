3f05dbc9f3d31b7179dd74b5e65ada3cd9edbcca
vivek rawat <vivekrawat@viveks-MacBook-Pro.local>
2020-03-22 15:22:11 +0530

    Created and added teacher ID to teacher struct.

--- models/teacher.go
+++ models/teacher.go
@@ -1,10 +1,17 @@
 package models
 
+// TeacherID is a unique identifier of the teacher
+type TeacherID struct {
+	Year   [4]byte `json:"yr"`
+	JoinNo [4]byte `json:"jNo"`
+}
+
 // Teacher is a struct to store teacher data
 type Teacher struct {
-	Name             Name    `json:"name"`
-	Subject          Subject `json:"subject"`
-	Capacity         int     `json:"capacity"`
-	FreePeriodPerDay int     `json:"freePeriodPerWeek"`
-	Classes          []Class `json:"classes"`
+	Name             Name      `json:"name"`
+	ID               TeacherID `json:"id"`
+	Subject          Subject   `json:"subject"`
+	Capacity         int       `json:"capacity"`
+	FreePeriodPerDay int       `json:"freePeriodPerWeek"`
+	Classes          []Class   `json:"classes"`
 }
