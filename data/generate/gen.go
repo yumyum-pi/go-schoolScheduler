54d812587e9b54c1be512049628d60202e8a49e1
vivek rawat <vivekrawat@viveks-MacBook-Pro.local>
2020-03-26 12:13:39 +0530

    function to analyze teacher created data.

--- data/generate/gen.go
+++ data/generate/gen.go
@@ -1,12 +1,44 @@
 package generate
 
-import "github.com/yumyum-pi/go-schoolScheduler/utils"
+import (
+	"fmt"
+
+	"github.com/yumyum-pi/go-schoolScheduler/models"
+	"github.com/yumyum-pi/go-schoolScheduler/requestlist"
+	"github.com/yumyum-pi/go-schoolScheduler/utils"
+)
+
+func printTeacher(ts *models.Teachers) {
+	fmt.Printf("Name\t\tID\t\t\tNOS\tCapicity\tSubjects\n")
+	for _, t := range *ts {
+		var subIDs models.SubjectIDs = t.SubjectCT
+		fmt.Printf("%v %v %v\t%v%v\t%v\t%v\t%v\n", t.Name.First, t.Name.Middle, t.Name.Last, t.ID.Year, t.ID.JoinNo, len(t.SubjectCT), t.Capacity, subIDs.Types())
+	}
+	fmt.Printf("\nNo. of Teacher=%v\n", len(*ts))
+}
 
 // Init Data
 func Init() {
 	classes := generateClasses()
-	// create requrestList
-	// generate teachers
+	// create Subject Requrest List
+	var srl requestlist.SubjectRL
+	srl.Create(&classes)
+
+	var trl requestlist.TeacherRL        // create Teacher Requrest List
+	trl.Create(&srl)                     // populate teacher request list
+	teacher := generateTeacherList(&trl) // generate teachers
+
+	//printTeacher(&teacher)
+
+	ac := []models.ClassAssigned{} // empty slice of class assigned struct
+	cap := dW * (pPD - fPPD)       // max capacity of the teacher set to default
+	// loop through all teacher and reset to default data
+	for _, t := range teacher {
+		t.ClassesAssigned = ac
+		t.Capacity = cap
+	}
+
 	// write to files
 	utils.WriteFile(utils.ResourceFilePath("classes"), classes)
+	utils.WriteFile(utils.ResourceFilePath("teacher"), teacher)
 }
