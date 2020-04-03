183ef1b9b361966b4ff52564158c28e73da0f562
vivek rawat <vivekrawat@viveks-MacBook-Pro.local>
2020-04-04 01:10:08 +0530

    PerintClasses added & made classes freePriod to 0

--- data/generate/gen.go
+++ data/generate/gen.go
@@ -8,18 +8,49 @@ import (
 	"github.com/yumyum-pi/go-schoolScheduler/utils"
 )
 
-func printTeacher(ts *models.Teachers) {
-	fmt.Printf("Name\t\tID\t\t\tNOS\tCapicity\tSubjects\n")
+// printTeachers prints the teacher data one by one on the console
+func printTeachers(ts *models.Teachers) {
 	for _, t := range *ts {
 		var subIDs models.SubjectIDs = t.SubjectCT
-		fmt.Printf("%v %v %v\t%v%v\t%v\t%v\t%v\n", t.Name.First, t.Name.Middle, t.Name.Last, t.ID.Year, t.ID.JoinNo, len(t.SubjectCT), t.Capacity, subIDs.Types())
+		fmt.Printf("Name=%v %v %v\tID=%v\tNOS=%v\tCapicity=%v\tSubjects=%v\n", t.Name.First, t.Name.Middle, t.Name.Last, t.ID.Bytes(), len(t.SubjectCT), t.Capacity, subIDs.Types())
 	}
 	fmt.Printf("\nNo. of Teacher=%v\n", len(*ts))
 }
 
+// printClasses prints the class data one by one on the console
+func printClasses(classes *models.Classes) {
+	for _, class := range *classes {
+		class.CalCap()
+		fmt.Println("No of free Periods=", class.NFreePeriod)
+		for _, sub := range class.Subjects {
+			fmt.Printf("cID=%v\tsID=%v\tReq=%v\n", class.ID.Bytes(), sub.ID.Bytes(), sub.ReqClasses)
+		}
+	}
+}
+
 // Init Data
 func Init() {
-	classes := generateClasses()
+	var classes models.Classes
+	classes = generateClasses()
+
+	// check class capacity and assign extra classes if requried
+	// loop through all the classes
+	for _, class := range classes {
+		class.CalCap()              // calcuate the capacity
+		n := class.NFreePeriod      // get the no. of free periods
+		subL := len(class.Subjects) // get no. of subject
+
+		// loop over the no. of free periods
+		for i := 0; i < n; i++ {
+			// loop the index no from 0 to subL
+			looper := (i / subL)
+			subIndex := i - (looper * subL)
+
+			// increase the no. of required classes
+			class.Subjects[subIndex].ReqClasses++
+		}
+	}
+
 	// create Subject Requrest List
 	var srl requestlist.SubjectRL
 	srl.Create(&classes)
@@ -28,15 +59,13 @@ func Init() {
 	trl.Create(&srl)                     // populate teacher request list
 	teacher := generateTeacherList(&trl) // generate teachers
 
-	//printTeacher(&teacher)
-
 	ac := []models.ClassAssigned{} // empty slice of class assigned struct
 	// loop through all teacher and reset to default data
 	for i := range teacher {
 		teacher[i].ClassesAssigned = ac
 		teacher[i].Capacity = models.MaxCap
 	}
-	// fmt.Println(teacher[12].Capacity)
+
 	// write to files
 	utils.WriteFile(utils.ResourceFilePath("classes"), classes)
 	utils.WriteFile(utils.ResourceFilePath("teachers"), teacher)
