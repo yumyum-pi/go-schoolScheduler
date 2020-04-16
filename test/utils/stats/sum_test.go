d0c9cf3af1f314222678782fe878671e33264f0f
vivek rawat <vivekrawat@viveks-MacBook-Pro.local>
2020-04-16 06:46:59 +0530

    move to to test folder.

--- test/utils/stats/sum_test.go
+++ test/utils/stats/sum_test.go
@@ -1,6 +1,10 @@
 package stats
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/yumyum-pi/go-schoolScheduler/internal/utils/stats"
+)
 
 var tIntSlice []int = []int{600, 470, 170, 430, 300}
 var sum int = 1970
@@ -8,21 +12,21 @@ var mean float64 = 394
 var variance float64 = 21704
 
 func TestSum(t *testing.T) {
-	s := Sum(tIntSlice)
+	s := stats.Sum(tIntSlice)
 	if s != sum {
 		t.Errorf("> Error: expected sum=%v, got sum=%v", sum, s)
 	}
 }
 
 func TestMean(t *testing.T) {
-	m := Mean(tIntSlice)
+	m := stats.Mean(tIntSlice)
 	if m != mean {
 		t.Errorf("> Error: expected mean=%v, got mean=%v", mean, m)
 	}
 }
 
 func TestVariance(t *testing.T) {
-	v := Variance(tIntSlice)
+	v := stats.Variance(tIntSlice)
 	if v != variance {
 		t.Errorf("> Error: expected Variance=%v, got Variance=%v", variance, v)
 	}
