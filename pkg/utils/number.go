9dff6b6ac28035d5adc13986b88dc14f04199fd7
vivek rawat <vivekrawat@viveks-MacBook-Pro.local>
2020-04-28 07:16:35 +0530

    restructured the files

---
+++ pkg/utils/number.go
@@ -0,0 +1,30 @@
+package utils
+
+import (
+	"math/rand"
+	"time"
+)
+
+// GenerateRandomInt return a random int with
+// given int and a factor
+func GenerateRandomInt(i int, factor int) int {
+	rand.Seed(time.Now().UnixNano())
+	n := rand.Intn(i * factor)
+	n /= factor
+
+	return n
+}
+
+// RangeInt is struct that has a two variable-min, max.
+// Using it's Random() method to get a random number between
+// min and max
+type RangeInt struct {
+	Min int
+	Max int
+}
+
+// Random return a random value between the range
+func (r *RangeInt) Random() int {
+	rand.Seed(time.Now().UnixNano())
+	return rand.Intn(r.Max-r.Min) + r.Min
+}
