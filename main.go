d8654b0bf089ac8747aac5e6bc2686d11d0a3caf
vivek rawat <vivekrawat@viveks-MacBook-Pro.local>
2020-04-28 19:46:19 +0530

    clean code and ignore test/inputs

--- main.go
+++ main.go
@@ -1,39 +1,7 @@
 package main
 
-import (
-	"fmt"
-
-	g "github.com/yumyum-pi/go-schoolScheduler/internal/generate"
-	"github.com/yumyum-pi/go-schoolScheduler/pkg/models"
-)
-
-func printTeacherData(ts *models.Teachers) {
-	for _, t := range *ts {
-		//fmt.Printf("TeacherID=%v\t\tFree=%v, s=%v\n", t.ID.Bytes(), t.Capacity, t.SubjectCT)
-		fmt.Printf("Free=%v\t\tTeacherID=%v\n", t.Capacity, t.ID.Bytes())
-	}
-}
-
-func printClassData(cs *[]models.Class) {
-	for _, c := range *cs {
-		c.CalCap()
-		ss := ""
-		for _, s := range c.Subjects {
-			ss += fmt.Sprintf("(sID%v r%v)", s.ID.Bytes(), s.Req)
-		}
-
-		fmt.Printf("cID%v fP%v %v\n", c.ID.Bytes(), c.Capacity, ss)
-	}
-}
-
-var c []models.Class
-var t models.Teachers
+import "github.com/yumyum-pi/go-schoolScheduler/cmd"
 
 func main() {
-	// generate classes and teachers
-	g.Create(&c, &t)
-	// start creating timetable
-	//generator.Init(c, t)
-
-	// get a timetable and print it
+	cmd.Execute()
 }
