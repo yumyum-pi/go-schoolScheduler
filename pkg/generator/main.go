f94da26b3259058069c125d5b3c1d67f3af8b03f
vivek rawat <vivekrawat@viveks-MacBook-Pro.local>
2020-04-30 16:00:45 +0530

    moved data check to decode. Change decode output.

--- pkg/generator/main.go
+++ pkg/generator/main.go
@@ -1,66 +1,20 @@
 package generator
 
 import (
-	"fmt"
-
 	"github.com/yumyum-pi/go-schoolScheduler/pkg/models"
 )
 
 const nGeneration = 4
 
-var tpCount map[byte]int = make(map[byte]int) // total period counter of a teacher
-
 // Start begin the generating process
 func Start(tt *models.TimeTable) (*models.TimeTable, error) {
-	b, max := (*tt).Decode()
-	bl := len(b)
-	var tID byte // teacher ID
-	var tp int   // total periods of a teacher
-	var ok bool  // if the teacher is persent in the map
-	for i := 0; i < bl; i += max {
-		for j := 0; j < max; j++ {
-			tID = b[(i + j)]
-
-			tp, ok = tpCount[tID]
-			if !ok {
-				tp = 0
-			}
-			tp++
-			tpCount[tID] = tp
-		}
-
+	s0, geneSize, err := (*tt).Decode()
+	if err != nil {
+		return nil, err
 	}
 
-	for _, tp := range tpCount {
-		if tp >= max {
-			return nil, fmt.Errorf("> Error: Data received is invalid")
-		}
-	}
 	var p Population
-	p.Init(&b, max)
+	p.Init(s0, geneSize)
 
 	return nil, nil
 }
-
-/*
-// Init starts the process
-func Init(tt *models.TimeTable) Chromosome {
-
-	var p Population
-
-	p.Init(&tt)
-	p.Sort()
-	p.PrintChromo()
-	p.Print()
-	fmt.Println("Init population")
-
-	for generationIndex := 0; generationIndex < nGeneration; generationIndex++ {
-		fmt.Println("population generation:", generationIndex)
-		p.Next()
-		p.Print()
-	}
-
-	// choose the best chromosome
-	return p.P[0]
-}
-*/
--- pkg/models/timetable.go
+++ pkg/models/timetable.go
@@ -1,7 +1,9 @@
 package models
 
-// Decode timetable data
-func (tt *TimeTable) Decode() (b []byte, maxcap int) {
+import "fmt"
+
+// Decode nucleotide sequence data
+func (tt *TimeTable) Decode() (*[]byte, int, error) {
 	// calculate the capacity
 	l := len((*tt).Period)
 	// get the length of the last byte
@@ -9,13 +11,58 @@ func (tt *TimeTable) Decode() (b []byte, maxcap int) {
 	cap := (l - 1) * 32
 	cap += lb
 
-	b = make([]byte, 0, cap)
+	s0 := make([]byte, 0, cap)
 	// loop through each package byte
 	for _, pkg := range (*tt).Period {
-		b = append(b, pkg...)
+		s0 = append(s0, pkg...)
 	}
 
 	// calculate maxcap
-	maxcap = int((*tt).NDays * (*tt).NPeriods)
-	return
+	geneSize := int((*tt).NDays * (*tt).NPeriods)
+
+	if e := checkData(&s0, geneSize); e != nil {
+		return nil, 0, e
+	}
+
+	return &s0, geneSize, nil
+}
+
+// checkData checks if the quantity of each nucleotide type is < geneSize
+// else thro error
+func checkData(s0 *[]byte, geneSize int) error {
+	s0l := len(*s0)
+
+	// total period counter of a teacher
+	geneMap := make(map[byte]int)
+
+	// variables to store values to avoid reassigning
+	var n byte  // single nucleotide
+	q := 0      // quantity of a nucleotide type
+	ok := false // if nucleotide exists in the map
+
+	// iterate over each gene to check all the nucleotides
+	for i := 0; i < s0l; i += geneSize {
+		// assign all the nucleotides type and their quantity in
+		// the gene to the respective maps
+		for j := 0; j < geneSize; j++ {
+			n = (*s0)[(i + j)] // get the nucleotide at the index
+
+			q, ok = geneMap[n] // check if nucleotide exist in the map
+			if !ok {
+				q = 0 // assign initial value
+			}
+			q++            // increase quantity of the nucleotide type
+			geneMap[n] = q // reassign to the map
+		}
+
+	}
+	// evalute the maps
+	for _, q := range geneMap {
+		// check the quantify of the nucleotide
+		if q >= geneSize {
+			return fmt.Errorf("> Error: Data received is invalid")
+		}
+	}
+
+	return nil
 }
