88cc134ed8f1391cbf915adda6f774da5efb5968
vivek rawat <vivekrawat@viveks-MacBook-Pro.local>
2020-04-30 13:07:13 +0530

    removed TestChromosome_illegalMutation test.

--- pkg/generator/population_test.go
+++ pkg/generator/population_test.go
@@ -10,27 +10,6 @@ import (
 
 var inputDir = "/Users/vivekrawat/go/src/github.com/yumyum-pi/go-schoolScheduler/test/inputs"
 
-func TestChromosome_illegalMutation(t *testing.T) {
-	rand.Seed(time.Now().UnixNano())
-	// get information from the file
-	pkgs := file.ReadRand(inputDir)
-
-	// decode the pkgs to ns0 and gene-size
-	ns0, gSize := pkgs.Decode()
-
-	var nc *chromosome
-
-	// create new chromosome
-	nc = newChromo(&ns0, gSize)
-
-	if e := illegalMutation(&ns0, &(nc.Nucleotides), gSize); e != nil {
-		t.Error(e)
-	}
-
-	// make illegal mutation
-	//
-}
-
 func TestPopulation_newChromo(t *testing.T) {
 	rand.Seed(time.Now().UnixNano())
 	// get information from the file
