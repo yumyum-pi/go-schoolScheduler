351e4a9ae3ca926055142e7365060f14a67a9305
vivek rawat <vivekrawat@viveks-MacBook-Pro.local>
2020-05-05 19:32:43 +0530

    updated crossOver test and added benchmark

--- pkg/generator/population_test.go
+++ pkg/generator/population_test.go
@@ -79,14 +79,6 @@ func TestCrossOver(t *testing.T) {
 	c0 := newChromo(ns0, gSize, 0, 0)
 	c1 := newChromo(ns0, gSize, 0, 0)
 
-	c0.CheckEM2()
-	c0.HandleEM1()
-	c0.HandleEM2()
-
-	c1.CheckEM2()
-	c1.HandleEM1()
-	c1.HandleEM2()
-
 	n0, n1 := crossOver(&c0.Sequence, &c1.Sequence, gSize)
 
 	if e := illegalMutation(ns0, n0, gSize); e != nil {
@@ -95,5 +87,21 @@ func TestCrossOver(t *testing.T) {
 
 	if e := illegalMutation(ns0, n1, gSize); e != nil {
 		t.Error(e)
+	}
+}
+
+func BenchmarkCrossOver(b *testing.B) {
+	rand.Seed(time.Now().UnixNano())
+	// get information from the file
+	pkgs := file.Read(inputDirMax)
+
+	// decode the pkgs to ns0 and gene-size
+	ns0, gSize, _ := pkgs.Decode()
+
+	// create new chromosome
+	c0 := newChromo(ns0, gSize, 0, 0)
+	c1 := newChromo(ns0, gSize, 0, 0)
+	for i := 0; i < b.N; i++ {
+		crossOver(&c0.Sequence, &c1.Sequence, gSize)
 	}
 }
