7717997b3bb36fc6ca2a03dbb6c829ae52eaeeb8
vivek rawat <vivekrawat@viveks-MacBook-Pro.local>
2020-05-06 17:17:38 +0530

    added grpc functions

--- pkg/models/sequence.pb.go
+++ pkg/models/sequence.pb.go
@@ -2,12 +2,16 @@
 // versions:
 // 	protoc-gen-go v1.22.0
 // 	protoc        v3.11.4
-// source: pkg/models/sequence.proto
+// source: sequence.proto
 
 package models
 
 import (
+	context "context"
 	proto "github.com/golang/protobuf/proto"
+	grpc "google.golang.org/grpc"
+	codes "google.golang.org/grpc/codes"
+	status "google.golang.org/grpc/status"
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
 	reflect "reflect"
@@ -37,7 +41,7 @@ type SequencePkgs struct {
 func (x *SequencePkgs) Reset() {
 	*x = SequencePkgs{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_pkg_models_sequence_proto_msgTypes[0]
+		mi := &file_sequence_proto_msgTypes[0]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -50,7 +54,7 @@ func (x *SequencePkgs) String() string {
 func (*SequencePkgs) ProtoMessage() {}
 
 func (x *SequencePkgs) ProtoReflect() protoreflect.Message {
-	mi := &file_pkg_models_sequence_proto_msgTypes[0]
+	mi := &file_sequence_proto_msgTypes[0]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -63,7 +67,7 @@ func (x *SequencePkgs) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use SequencePkgs.ProtoReflect.Descriptor instead.
 func (*SequencePkgs) Descriptor() ([]byte, []int) {
-	return file_pkg_models_sequence_proto_rawDescGZIP(), []int{0}
+	return file_sequence_proto_rawDescGZIP(), []int{0}
 }
 
 func (x *SequencePkgs) GetGSize() int32 {
@@ -80,43 +84,42 @@ func (x *SequencePkgs) GetPkgs() [][]byte {
 	return nil
 }
 
-var File_pkg_models_sequence_proto protoreflect.FileDescriptor
+var File_sequence_proto protoreflect.FileDescriptor
 
-var file_pkg_models_sequence_proto_rawDesc = []byte{
-	0x0a, 0x19, 0x70, 0x6b, 0x67, 0x2f, 0x6d, 0x6f, 0x64, 0x65, 0x6c, 0x73, 0x2f, 0x73, 0x65, 0x71,
-	0x75, 0x65, 0x6e, 0x63, 0x65, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x12, 0x06, 0x6d, 0x6f, 0x64,
-	0x65, 0x6c, 0x73, 0x22, 0x38, 0x0a, 0x0c, 0x53, 0x65, 0x71, 0x75, 0x65, 0x6e, 0x63, 0x65, 0x50,
-	0x6b, 0x67, 0x73, 0x12, 0x14, 0x0a, 0x05, 0x67, 0x53, 0x69, 0x7a, 0x65, 0x18, 0x01, 0x20, 0x01,
-	0x28, 0x05, 0x52, 0x05, 0x67, 0x53, 0x69, 0x7a, 0x65, 0x12, 0x12, 0x0a, 0x04, 0x70, 0x6b, 0x67,
-	0x73, 0x18, 0x02, 0x20, 0x03, 0x28, 0x0c, 0x52, 0x04, 0x70, 0x6b, 0x67, 0x73, 0x32, 0x46, 0x0a,
-	0x08, 0x53, 0x65, 0x71, 0x75, 0x65, 0x6e, 0x63, 0x65, 0x12, 0x3a, 0x0a, 0x0a, 0x47, 0x65, 0x6e,
-	0x65, 0x72, 0x61, 0x74, 0x65, 0x54, 0x54, 0x12, 0x14, 0x2e, 0x6d, 0x6f, 0x64, 0x65, 0x6c, 0x73,
-	0x2e, 0x53, 0x65, 0x71, 0x75, 0x65, 0x6e, 0x63, 0x65, 0x50, 0x6b, 0x67, 0x73, 0x1a, 0x14, 0x2e,
-	0x6d, 0x6f, 0x64, 0x65, 0x6c, 0x73, 0x2e, 0x53, 0x65, 0x71, 0x75, 0x65, 0x6e, 0x63, 0x65, 0x50,
-	0x6b, 0x67, 0x73, 0x22, 0x00, 0x42, 0x34, 0x5a, 0x32, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e,
-	0x63, 0x6f, 0x6d, 0x2f, 0x79, 0x75, 0x6d, 0x79, 0x75, 0x6d, 0x2d, 0x70, 0x69, 0x2f, 0x67, 0x6f,
-	0x2d, 0x73, 0x63, 0x68, 0x6f, 0x6f, 0x6c, 0x53, 0x63, 0x68, 0x65, 0x64, 0x75, 0x6c, 0x65, 0x72,
-	0x2f, 0x70, 0x6b, 0x67, 0x2f, 0x6d, 0x6f, 0x64, 0x65, 0x6c, 0x73, 0x62, 0x06, 0x70, 0x72, 0x6f,
-	0x74, 0x6f, 0x33,
+var file_sequence_proto_rawDesc = []byte{
+	0x0a, 0x0e, 0x73, 0x65, 0x71, 0x75, 0x65, 0x6e, 0x63, 0x65, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f,
+	0x12, 0x06, 0x6d, 0x6f, 0x64, 0x65, 0x6c, 0x73, 0x22, 0x38, 0x0a, 0x0c, 0x53, 0x65, 0x71, 0x75,
+	0x65, 0x6e, 0x63, 0x65, 0x50, 0x6b, 0x67, 0x73, 0x12, 0x14, 0x0a, 0x05, 0x67, 0x53, 0x69, 0x7a,
+	0x65, 0x18, 0x01, 0x20, 0x01, 0x28, 0x05, 0x52, 0x05, 0x67, 0x53, 0x69, 0x7a, 0x65, 0x12, 0x12,
+	0x0a, 0x04, 0x70, 0x6b, 0x67, 0x73, 0x18, 0x02, 0x20, 0x03, 0x28, 0x0c, 0x52, 0x04, 0x70, 0x6b,
+	0x67, 0x73, 0x32, 0x46, 0x0a, 0x08, 0x53, 0x65, 0x71, 0x75, 0x65, 0x6e, 0x63, 0x65, 0x12, 0x3a,
+	0x0a, 0x0a, 0x47, 0x65, 0x6e, 0x65, 0x72, 0x61, 0x74, 0x65, 0x54, 0x54, 0x12, 0x14, 0x2e, 0x6d,
+	0x6f, 0x64, 0x65, 0x6c, 0x73, 0x2e, 0x53, 0x65, 0x71, 0x75, 0x65, 0x6e, 0x63, 0x65, 0x50, 0x6b,
+	0x67, 0x73, 0x1a, 0x14, 0x2e, 0x6d, 0x6f, 0x64, 0x65, 0x6c, 0x73, 0x2e, 0x53, 0x65, 0x71, 0x75,
+	0x65, 0x6e, 0x63, 0x65, 0x50, 0x6b, 0x67, 0x73, 0x22, 0x00, 0x42, 0x34, 0x5a, 0x32, 0x67, 0x69,
+	0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x79, 0x75, 0x6d, 0x79, 0x75, 0x6d, 0x2d,
+	0x70, 0x69, 0x2f, 0x67, 0x6f, 0x2d, 0x73, 0x63, 0x68, 0x6f, 0x6f, 0x6c, 0x53, 0x63, 0x68, 0x65,
+	0x64, 0x75, 0x6c, 0x65, 0x72, 0x2f, 0x70, 0x6b, 0x67, 0x2f, 0x6d, 0x6f, 0x64, 0x65, 0x6c, 0x73,
+	0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
-	file_pkg_models_sequence_proto_rawDescOnce sync.Once
-	file_pkg_models_sequence_proto_rawDescData = file_pkg_models_sequence_proto_rawDesc
+	file_sequence_proto_rawDescOnce sync.Once
+	file_sequence_proto_rawDescData = file_sequence_proto_rawDesc
 )
 
-func file_pkg_models_sequence_proto_rawDescGZIP() []byte {
-	file_pkg_models_sequence_proto_rawDescOnce.Do(func() {
-		file_pkg_models_sequence_proto_rawDescData = protoimpl.X.CompressGZIP(file_pkg_models_sequence_proto_rawDescData)
+func file_sequence_proto_rawDescGZIP() []byte {
+	file_sequence_proto_rawDescOnce.Do(func() {
+		file_sequence_proto_rawDescData = protoimpl.X.CompressGZIP(file_sequence_proto_rawDescData)
 	})
-	return file_pkg_models_sequence_proto_rawDescData
+	return file_sequence_proto_rawDescData
 }
 
-var file_pkg_models_sequence_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
-var file_pkg_models_sequence_proto_goTypes = []interface{}{
+var file_sequence_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
+var file_sequence_proto_goTypes = []interface{}{
 	(*SequencePkgs)(nil), // 0: models.SequencePkgs
 }
-var file_pkg_models_sequence_proto_depIdxs = []int32{
+var file_sequence_proto_depIdxs = []int32{
 	0, // 0: models.Sequence.GenerateTT:input_type -> models.SequencePkgs
 	0, // 1: models.Sequence.GenerateTT:output_type -> models.SequencePkgs
 	1, // [1:2] is the sub-list for method output_type
@@ -126,13 +129,13 @@ var file_pkg_models_sequence_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for field type_name
 }
 
-func init() { file_pkg_models_sequence_proto_init() }
-func file_pkg_models_sequence_proto_init() {
-	if File_pkg_models_sequence_proto != nil {
+func init() { file_sequence_proto_init() }
+func file_sequence_proto_init() {
+	if File_sequence_proto != nil {
 		return
 	}
 	if !protoimpl.UnsafeEnabled {
-		file_pkg_models_sequence_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
+		file_sequence_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*SequencePkgs); i {
 			case 0:
 				return &v.state
@@ -149,18 +152,98 @@ func file_pkg_models_sequence_proto_init() {
 	out := protoimpl.TypeBuilder{
 		File: protoimpl.DescBuilder{
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
-			RawDescriptor: file_pkg_models_sequence_proto_rawDesc,
+			RawDescriptor: file_sequence_proto_rawDesc,
 			NumEnums:      0,
 			NumMessages:   1,
 			NumExtensions: 0,
 			NumServices:   1,
 		},
-		GoTypes:           file_pkg_models_sequence_proto_goTypes,
-		DependencyIndexes: file_pkg_models_sequence_proto_depIdxs,
-		MessageInfos:      file_pkg_models_sequence_proto_msgTypes,
+		GoTypes:           file_sequence_proto_goTypes,
+		DependencyIndexes: file_sequence_proto_depIdxs,
+		MessageInfos:      file_sequence_proto_msgTypes,
 	}.Build()
-	File_pkg_models_sequence_proto = out.File
-	file_pkg_models_sequence_proto_rawDesc = nil
-	file_pkg_models_sequence_proto_goTypes = nil
-	file_pkg_models_sequence_proto_depIdxs = nil
+	File_sequence_proto = out.File
+	file_sequence_proto_rawDesc = nil
+	file_sequence_proto_goTypes = nil
+	file_sequence_proto_depIdxs = nil
+}
+
+// Reference imports to suppress errors if they are not otherwise used.
+var _ context.Context
+var _ grpc.ClientConnInterface
+
+// This is a compile-time assertion to ensure that this generated file
+// is compatible with the grpc package it is being compiled against.
+const _ = grpc.SupportPackageIsVersion6
+
+// SequenceClient is the client API for Sequence service.
+//
+// For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
+type SequenceClient interface {
+	GenerateTT(ctx context.Context, in *SequencePkgs, opts ...grpc.CallOption) (*SequencePkgs, error)
+}
+
+type sequenceClient struct {
+	cc grpc.ClientConnInterface
+}
+
+func NewSequenceClient(cc grpc.ClientConnInterface) SequenceClient {
+	return &sequenceClient{cc}
+}
+
+func (c *sequenceClient) GenerateTT(ctx context.Context, in *SequencePkgs, opts ...grpc.CallOption) (*SequencePkgs, error) {
+	out := new(SequencePkgs)
+	err := c.cc.Invoke(ctx, "/models.Sequence/GenerateTT", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+// SequenceServer is the server API for Sequence service.
+type SequenceServer interface {
+	GenerateTT(context.Context, *SequencePkgs) (*SequencePkgs, error)
+}
+
+// UnimplementedSequenceServer can be embedded to have forward compatible implementations.
+type UnimplementedSequenceServer struct {
+}
+
+func (*UnimplementedSequenceServer) GenerateTT(context.Context, *SequencePkgs) (*SequencePkgs, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GenerateTT not implemented")
+}
+
+func RegisterSequenceServer(s *grpc.Server, srv SequenceServer) {
+	s.RegisterService(&_Sequence_serviceDesc, srv)
+}
+
+func _Sequence_GenerateTT_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(SequencePkgs)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(SequenceServer).GenerateTT(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/models.Sequence/GenerateTT",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(SequenceServer).GenerateTT(ctx, req.(*SequencePkgs))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+var _Sequence_serviceDesc = grpc.ServiceDesc{
+	ServiceName: "models.Sequence",
+	HandlerType: (*SequenceServer)(nil),
+	Methods: []grpc.MethodDesc{
+		{
+			MethodName: "GenerateTT",
+			Handler:    _Sequence_GenerateTT_Handler,
+		},
+	},
+	Streams:  []grpc.StreamDesc{},
+	Metadata: "sequence.proto",
 }
