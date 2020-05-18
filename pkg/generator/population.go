536e32e8be8d0c1bfdd98fbea1fc98de1093e08b
vivek rawat <vivekrawat@viveks-MacBook-Pro.local>
2020-05-18 20:11:28 +0530

    =add goroutine to population

--- pkg/generator/population.go
+++ pkg/generator/population.go
@@ -4,11 +4,12 @@ import (
 	"fmt"
 	"math/rand"
 	"sort"
+	"sync"
 )
 
 const pSize = 32                // population size
 const p2Size = pSize / 2        // half of population size
-const p4Size = pSize / 4        // quater of population size
+const p4Size = pSize / 4        // quarter of population size
 const p34Size = p4Size + p2Size // 3/4 of population size
 
 // Population is a fixed array of size 64 containing chromosomes
@@ -18,17 +19,28 @@ type Population struct {
 	gSize int
 }
 
-// Init create the initail population
+// Init create the initial population
 func (p *Population) Init(ns0 *[]byte, gSize int) {
-	//nL := len(*ns0) // length of nucleotides
+	// assign the initial values
 	(*p).ns0 = ns0
 	(*p).gSize = gSize
-	// make a new chromosome var
+
+	var wg sync.WaitGroup
 
 	for i := 0; i < pSize; i++ {
-		(*p).P[i] = *newChromo(ns0, (*p).gSize, 0, i)
+		wg.Add(1)
+
+		// run the function concurrently
+		go func(i int) {
+			(*p).P[i] = *newChromo((*p).ns0, (*p).gSize, 0, i-p34Size)
+			wg.Done()
+		}(i)
 	}
 
+	// waiting for all the task to get over
+	wg.Wait()
+
+	// sort the population on fitness
 	(*p).Sort()
 }
 
@@ -73,75 +85,115 @@ func (p *Population) Print(details bool) {
 
 // Next creates the next gene of chromosome
 func (p *Population) Next(g int) {
-	(*p).CrossOver(g)
-	(*p).Mutate(g)
-	(*p).New(g)
+	var wg sync.WaitGroup
+	wg.Add(3)
+
+	go func() {
+		(*p).CrossOver(g)
+		wg.Done()
+	}()
+
+	go func() {
+		(*p).Mutate(g)
+		wg.Done()
+	}()
+
+	go func() {
+		(*p).New(g)
+		wg.Done()
+	}()
+	wg.Wait()
+
 	(*p).Sort()
 }
 
-// CrossOver creates new chromosomes form the existing chromosomes
-// by cross overing the genes
+// Crossover creates new chromosomes form the existing chromosomes
+// by mixing the genes of two chromosome
 func (p *Population) CrossOver(g int) {
+	var wg sync.WaitGroup
+
 	// loop through 1/4 for the list in pair of two
 	for i := 0; i < p4Size; i += 2 {
-		// get the new cross over sequences
-		ns0, ns1 := crossOver(&(*p).P[i].Sequence, &(*p).P[i+1].Sequence, (*p).P[i].GeneSize)
+		wg.Add(1)
+		go func(i int) {
+			// get the new cross over sequences
+			ns0, ns1 := crossOver(&(*p).P[i].Sequence, &(*p).P[i+1].Sequence, (*p).P[i].GeneSize)
 
-		// create chromosomes
-		var c0, c1 chromosome
-		c0.GeneSize = (*p).gSize
-		c1.GeneSize = (*p).gSize
+			// create chromosomes
+			var c0, c1 chromosome
+			c0.GeneSize = (*p).gSize
+			c1.GeneSize = (*p).gSize
 
-		nL := len(*ns0) // length of nucleotides
+			nL := len(*ns0) // length of nucleotides
 
-		c0.Sequence = append((*ns0)[:0:0], (*ns0)...) // copy the value
-		c1.Sequence = append((*ns1)[:0:0], (*ns1)...) // copy the value
+			c0.Sequence = append((*ns0)[:0:0], (*ns0)...) // copy the value
+			c1.Sequence = append((*ns1)[:0:0], (*ns1)...) // copy the value
 
-		c0.ErrSequence = make([]byte, nL, nL)
-		c1.ErrSequence = make([]byte, nL, nL)
+			c0.ErrSequence = make([]byte, nL, nL)
+			c1.ErrSequence = make([]byte, nL, nL)
 
-		c0.lSequence = nL
-		c1.lSequence = nL
+			c0.lSequence = nL
+			c1.lSequence = nL
 
-		// handle error
-		c0.CheckEM2()
-		c1.CheckEM2()
+			// handle error
+			c0.CheckEM2()
+			c1.CheckEM2()
 
-		c0.HandleEM1()
-		c1.HandleEM1()
+			c0.HandleEM1()
+			c1.HandleEM1()
 
-		c0.HandleEM2()
-		c1.HandleEM2()
+			c0.HandleEM2()
+			c1.HandleEM2()
 
-		c0.CalFitness()
-		c1.CalFitness()
+			c0.CalFitness()
+			c1.CalFitness()
 
-		// generate code name
-		c0.GenCode = fmt.Sprintf("%02dC%02d:%04v", g, i, c0.nErr)
-		c1.GenCode = fmt.Sprintf("%02dC%02d:%04v", g, i+1, c1.nErr)
+			// generate code name
+			c0.GenCode = fmt.Sprintf("%02dC%02d:%04v", g, i, c0.nErr)
+			c1.GenCode = fmt.Sprintf("%02dC%02d:%04v", g, i+1, c1.nErr)
 
-		// assign to the populations at index after the fittest
-		// population
-		(*p).P[p4Size+i] = c0
-		(*p).P[p4Size+i+1] = c1
+			// assign to the populations at index after the fittest
+			// population
+			(*p).P[p4Size+i] = c0
+			(*p).P[p4Size+i+1] = c1
+
+			wg.Done()
+		}(i)
 	}
+	wg.Wait()
 }
 
 // New creates new chromosomes from the source sequence
 func (p *Population) New(g int) {
+	var wg sync.WaitGroup
 	// loop through 1/4 for the list
 	for i := p34Size; i < pSize; i++ {
-		(*p).P[i] = *newChromo((*p).ns0, (*p).gSize, g, i-p34Size)
+		wg.Add(1)
+
+		go func(i int) {
+			(*p).P[i] = *newChromo((*p).ns0, (*p).gSize, g, i-p34Size)
+			wg.Done()
+		}(i)
 	}
+	wg.Wait()
 }
 
 // Mutate creates new chromosomes by changing
 func (p *Population) Mutate(g int) {
+	var wg sync.WaitGroup
+
 	for i := p2Size; i < p34Size; i++ {
-		(*p).P[i].HandleEM2()
-		(*p).P[i].CheckEM2()
-		(*p).P[i].GenCode = fmt.Sprintf("%02dM%02d:%04v", g, i-p2Size, (*p).P[i].nErr)
+		wg.Add(1)
+		go func(i int) {
+			(*p).P[i].HandleEM2()
+			(*p).P[i].CheckEM2()
+			(*p).P[i].GenCode = fmt.Sprintf("%02dM%02d:%04v", g, i-p2Size, (*p).P[i].nErr)
+
+			wg.Done()
+		}(i)
 	}
+
+	wg.Wait()
 }
 
 // Sort will sort the data by fitness
@@ -154,7 +206,7 @@ func (p *Population) Sort() {
 	(*p).P = nc
 }
 
-// crossOver creates new nucleotide sequence by exchanging genes between two
+// crossover creates new nucleotide sequence by exchanging genes between two
 // nucleotide sequence
 func crossOver(s0, s1 *[]byte, gSize int) (*[]byte, *[]byte) {
 	sl := len((*s0))
